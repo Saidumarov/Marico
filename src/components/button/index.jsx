4aa75e2349ab3e598df224a4847d8cb0285c3bbb
Saidumarov <saidumarovjafarxon@gmail.com>
2024-03-10 00:01:02 -0300

    .

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -4,10 +4,10 @@ const Button = ({ title, color, bg, border, w, h, className }) => {
     <>
       <button
         className={className}
+        id={border ? "btn" : ""}
         style={{
           color: color ? "white" : "rgba(112, 112, 112, 1)",
           backgroundColor: bg ? "rgb(47, 110, 247)" : "rgb(0, 0, 0)",
-          border: border ? "1px" : "0",
           width: w,
           height: h,
         }}
--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -1,5 +1,69 @@
+import Button from "../button";
+import "./index.scss";
+import img from "../../assets/images/Dot.png";
+import { Svg1, Svg2, Svg3 } from "../../constants";
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <>
+      <main>
+        <div className="container">
+          <h2 className="text">Own your audience.</h2>
+          <h2 className="text1">So you don't lose them</h2>
+          <div className="btns_w">
+            <h3>Turn your audience into email and text message subscribers.</h3>
+            <div className="buttons">
+              <Button
+                title={"View A Demo"}
+                className={"right"}
+                color={"w"}
+                w={"48%"}
+                h={"55px"}
+                bg={"."}
+              />
+              <Button
+                title={"Get Started Now"}
+                className={"left"}
+                w={"48%"}
+                h={"55px"}
+                border={"1px"}
+              />
+            </div>
+            <div className="dot_w">
+              <img src={img} alt="" className="dot" />
+              <p>
+                <span className="count"> 1000+</span>
+                <span> creators have already started</span>
+              </p>
+            </div>
+          </div>
+          <div className="marico">
+            <h2>Why Creators Love Marico </h2>
+            <div className="marico_wrapper">
+              <div className="card_marico">
+                <h3>
+                  <Svg1 /> Reduced Anxiety
+                </h3>
+                <p>Never worry about losing your audience.</p>
+              </div>
+              <div className="card_marico">
+                <h3>
+                  <Svg2 />
+                  Lower Workload
+                </h3>
+                <p>Just share one link. We'll handle the rest.</p>
+              </div>
+              <div className="card_marico">
+                <h3>
+                  <Svg3 /> More Time
+                </h3>
+                <p>Spend less time on marketing tools</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
+  );
 };
 
 export default Main;
