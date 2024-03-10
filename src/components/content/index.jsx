d978b36f26e8181527d6e213f0799c0aa64af2ac
Saidumarov <saidumarovjafarxon@gmail.com>
2024-03-10 11:56:18 -0300

    .

--- src/components/content/index.jsx
+++ src/components/content/index.jsx
@@ -1,5 +1,67 @@
+import { Svg4, Svg5 } from "../../constants";
+import Button from "../button";
+import "./index.scss";
 const Content = () => {
-  return <div>Content</div>;
+  return (
+    <>
+      <section>
+        <div className="container">
+          <div className="content">
+            <h4>STEP 1</h4>
+            <h2>Connect Your Content</h2>
+            <p className="p">
+              Bring all of your content together and get a Homepage that
+              automatically updates whenever you create anywhere online.
+            </p>
+            <Button
+              title={"Get Started Now"}
+              className={"bottom"}
+              w={"265px"}
+              h={"55px"}
+              border={"1px"}
+            />
+          </div>
+          <div className="content_w">
+            <div className="content_left">
+              <h4>Your Homepage</h4>
+              <h2>Your Content.</h2>
+              <h2>
+                All in <span>One Spot</span>
+              </h2>
+              <p className="pone">
+                <Svg4 /> Bring all of your content together into one homepage.
+              </p>
+
+              <p>
+                <Svg5 /> Your page automatically updates whenever you create.
+              </p>
+              <h3>
+                Turn your audience into email and text message subscribers.
+              </h3>
+              <div className="buttons">
+                <Button
+                  title={"View A Demo"}
+                  className={"right"}
+                  color={"w"}
+                  w={"48%"}
+                  h={"50px"}
+                  bg={"."}
+                />
+                <Button
+                  title={"Get Started Now"}
+                  className={"left"}
+                  w={"48%"}
+                  h={"50px"}
+                  border={"1px"}
+                />
+              </div>
+            </div>
+            <div className="content_right"></div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Content;
--- src/components/share/index.jsx
+++ src/components/share/index.jsx
@@ -1,5 +1,62 @@
+import { Svg6 } from "../../constants";
+import Button from "../button";
+import "./index.scss";
 const Share = () => {
-  return <div>Share</div>;
+  return (
+    <>
+      <section>
+        <div className="container">
+          <div className="share_w">
+            <h4>STEP 2</h4>
+            <h2>Share Your Homepage</h2>
+            <p className="p">
+              Share your Wavium homepage link with your followers, and we'll
+              handle the rest.
+            </p>
+          </div>
+          <div className="share_item_w">
+            <div className="share_left">
+              <h4>One Link</h4>
+              <h2>
+                <span>ALL You Create.</span> <br />
+                One Link
+              </h2>
+              <div className="imgbg">
+                <h4>9:41</h4>
+                <div className="phone_w">
+                  <div className="phone_w_item">
+                    <p className="a">AA</p>
+                    <p>
+                      <Svg6 />
+                    </p>
+                    <p className="text_p">yourname.wavium.co</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="share_right">
+              <h4>Collect Subscribers</h4>
+              <h2>
+                <span>
+                  Emails. <br /> Phone #s.
+                </span>
+                <br />
+                All Yours.
+              </h2>
+            </div>
+          </div>
+          <Button
+            title={"Get Started "}
+            className={"sharebtn"}
+            color={"w"}
+            w={"283px"}
+            h={"55px"}
+            bg={"."}
+          />
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Share;
